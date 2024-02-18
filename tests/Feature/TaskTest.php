ecc91fe1cd36c3df61001990432010a779b8a15e
akira <yuhstamtch@yahoo.co.jp>
2024-02-18 16:53:50 +0900

    testクラスの使用開始

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -5,17 +5,17 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class TaskTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
     public function test_example()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        $response->assertOk();
     }
 }
